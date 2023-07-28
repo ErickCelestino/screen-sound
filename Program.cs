3cfb7154f8ae0c300c678094b2ca6bcc8deb0195
erick Celestino <erickcelestimo@gmail.com>
2023-07-28 19:38:15 -0300

    feat: add cases for ShowOptionsMenu

--- Program.cs
+++ Program.cs
@@ -19,6 +19,25 @@ void ShowOptionsMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string chosenOption = Console.ReadLine()!;
+    int chosenNumericOption = int.Parse(chosenOption);
+    switch (chosenNumericOption)
+    {
+        case 1: Console.WriteLine("Você digitou a opção " + chosenOption);
+            break;
+        case 2: Console.WriteLine("Você digitou a opção " + chosenOption);
+            break;
+        case 3: Console.WriteLine("Você digitou a opção " + chosenOption);
+            break;
+        case 4: Console.WriteLine("Você digitou a opção " + chosenOption);
+            break;
+        case -1: Console.WriteLine("Tchau tchau =)");
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
 }
 
 ShowWelomeMessage(welcome);
