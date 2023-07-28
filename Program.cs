08f8db85bdb3306d7f289ce511b49bfe4fdab5e8
erick Celestino <erickcelestimo@gmail.com>
2023-07-28 19:20:38 -0300

    feat: add ShowOptionsMenu and ShowWelcomeMesssage

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿String welcome = "Welcome to Screen Sound";
-Console.WriteLine(welcome);
+﻿string  welcome = "Welcome to Screen Sound";
+
+void ShowWelomeMessage(string messagem)
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(messagem);
+}
+
+void ShowOptionsMenu()
+{
+    Console.Write("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
+
+ShowWelomeMessage(welcome);
+ShowOptionsMenu();
