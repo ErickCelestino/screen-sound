1d4df17d537d524e61dc39821e048bad3511be55
erick Celestino <erickcelestimo@gmail.com>
2023-07-30 13:00:55 -0300

    feat: add Album class

---
+++ Album.cs
@@ -0,0 +1,21 @@
+class Album
+{
+    private List<Music> musics = new List<Music>();
+    public string Name { get; set; }
+    public int TotalDuration => musics.Sum(m => m.Duration);
+
+    public  void AddMusic(Music music) 
+    {
+        musics.Add(music);
+    } 
+
+    public void ShowMusicsFromAlgum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Name}");
+         foreach(var music in musics)
+         {
+            Console.WriteLine($"Música: {music.Name}");
+         }
+         Console.WriteLine($"\nPara ouvir este álbum inteiro você preisa de {TotalDuration} segundos");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿Music music1 = new Music();
-music1.Name = "Faroeste caboclo";
-music1.Artist = "Legião Urbana";
-music1.Duration = 1800;
-music1.Avaliable = true;
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Name = "A night at the opera";
+
+Music music1 = new Music();
+music1.Name = "Love of my life";
+music1.Duration = 213;
 
 Music music2 = new Music();
-music2.Name = "Garota";
-music2.Artist = "Froid";
-music2.Duration = 360;
-music2.Avaliable = false;
+music2.Name = "Bohemian Rhapsody";
+music2.Duration = 354;
 
-music1.ShowTecnicalFiche();
-music2.ShowTecnicalFiche();
+albumDoQueen.AddMusic(music1);
+albumDoQueen.AddMusic(music2);
+albumDoQueen.ShowMusicsFromAlgum();
