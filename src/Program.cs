99f4800e5879b13f09c6e1913f0b1bebd392e956
erick Celestino <erickcelestimo@gmail.com>
2023-07-30 20:28:03 -0300

    feat: add class Episode

--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,30 @@
-﻿Band queen = new Band("Queen");
+﻿// Band queen = new Band("Queen");
 
-Album albumDoQueen = new Album("A night at the opera");
+// Album albumDoQueen = new Album("A night at the opera");
 
-Music music1 = new Music(queen,"Love of my life")
-{
-    Duration = 213,
-    Avaliable = true
-};
+// Music music1 = new Music(queen,"Love of my life")
+// {
+//     Duration = 213,
+//     Avaliable = true
+// };
 
 
-Music music2 = new Music(queen,"Bohemian Rhapsody")
-{
-    Duration = 354,
-    Avaliable = false
-};
+// Music music2 = new Music(queen,"Bohemian Rhapsody")
+// {
+//     Duration = 354,
+//     Avaliable = false
+// };
 
-albumDoQueen.AddMusic(music1);
-albumDoQueen.AddMusic(music2);
-queen.AddAlbum(albumDoQueen);
+// albumDoQueen.AddMusic(music1);
+// albumDoQueen.AddMusic(music2);
+// queen.AddAlbum(albumDoQueen);
 
-music1.ShowTecnicalFiche();
-music2.ShowTecnicalFiche();
-albumDoQueen.ShowMusicsFromAlgum();
-queen.ShowDiscography();
+// music1.ShowTecnicalFiche();
+// music2.ShowTecnicalFiche();
+// albumDoQueen.ShowMusicsFromAlgum();
+// queen.ShowDiscography();
+
+Episode ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AddGuest("Erick");
+ep1.AddGuest("Jovana");
+Console.WriteLine(ep1.Resume);
